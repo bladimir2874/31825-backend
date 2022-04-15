eda2aed0da49cf58442db00700bd9509c4b46223
Bladimir Andrade <ab_bladimir@hotmail.com>
2022-04-15 08:07:03 -0500

    clase 01

--- 01-desafioClase01/container.js
+++ 01-desafioClase01/container.js
@@ -1,56 +1,55 @@
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = class Container {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
-
   async save(object) {
     try {
-      const data = await fs.promises.readFile(`./${this.name}`, 'utf-8')
-      const result = JSON.parse(data)
-      const newData = [...result]
+      const data = await fs.promises.readFile(`./${this.name}`, "utf-8");
+      const result = JSON.parse(data);
+      const newData = [...result];
       const payload = {
         title: object.title,
         price: object.price,
         thumbnail: object.thumbnail,
-        id: result.length + 1
-      }
-      newData.push(payload)
-      await fs.promises.writeFile(`./${this.name}`, JSON.stringify(newData))
-      return console.log('guardado\n')
+        id: result.length + 1,
+      };
+      newData.push(payload);
+      await fs.promises.writeFile(`./${this.name}`, JSON.stringify(newData));
+      return console.log("guardado\n");
     } catch (err) {
-      console.log('[falla al guardar]', err)
-      await fs.promises.writeFile(`./${this.name}`, '[]')
-      console.log('archivo creado, vuelve a intentar\n')
+      console.log("[falla al guardar]", err);
+      await fs.promises.writeFile(`./${this.name}`, "[]");
+      console.log("archivo creado, vuelve a intentar\n");
     }
   }
   async getById(id) {
-    const data = fs.readFileSync(`${this.name}`)
-    const dataJson = JSON.parse(data)
-    return dataJson.find((item) => item.id === id)
+    const data = fs.readFileSync(`${this.name}`);
+    const dataJson = JSON.parse(data);
+    return dataJson.find((item) => item.id === id);
   }
   async getAll() {
     try {
-      const data = fs.readFileSync(`./${this.name}`, 'utf-8')
-      const dataJson = JSON.parse(data)
-      return dataJson
+      const data = fs.readFileSync(`./${this.name}`, "utf-8");
+      const dataJson = JSON.parse(data);
+      return dataJson;
     } catch (err) {
-      return err
+      return err;
     }
   }
   deleteById(id) {
-    const data = fs.readFileSync(`${this.name}`)
-    const dataJson = JSON.parse(data)
-    const newData = dataJson.filter((item) => item.id !== id)
-    fs.writeFileSync(`${this.name}`, JSON.stringify(newData))
+    const data = fs.readFileSync(`${this.name}`);
+    const dataJson = JSON.parse(data);
+    const newData = dataJson.filter((item) => item.id !== id);
+    fs.writeFileSync(`${this.name}`, JSON.stringify(newData));
   }
   async deleteAll() {
     try {
-      await fs.promises.unlink(`./${this.name}`)
-      return 'borrado\n'
+      await fs.promises.unlink(`./${this.name}`);
+      return "borrado\n";
     } catch (error) {
-      return `[Falla al borrar] ${error}`
+      return `[Falla al borrar] ${error}`;
     }
   }
-}
+};
